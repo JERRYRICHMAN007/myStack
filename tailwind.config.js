5049dd84ca41b979ef8ae8d7fa18794159704b40
JERRYRICHMAN007 <jerryrichman70@gmail.com>
2025-07-22 11:16:44 +0000

    Initial push of MS  Frontend 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        sm: "calc(var(--radius) - 4px)",
+        md: "calc(var(--radius) - 2px)",
+        lg: "var(--radius)",
+        xl: "calc(var(--radius) + 4px)",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    // Add other plugins here if needed
+  ],
+};
